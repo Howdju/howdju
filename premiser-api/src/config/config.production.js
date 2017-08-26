eb5ff691c332ccd56dce786ae34a50bcb0277af3
Carl G <carl.gieringer@gmail.com>
2017-08-25 21:58:05 -0400

    Fix CORS origins

--- premiser-api/src/config/config.production.js
+++ premiser-api/src/config/config.production.js
@@ -4,8 +4,8 @@ module.exports = {
     'http://www.howdju.com',
     'https://www.howdju.com',
 
-    'http://pre-prod.www.howdju.com/',
-    'https://pre-prod.www.howdju.com/',
+    'http://pre-prod.www.howdju.com',
+    'https://pre-prod.www.howdju.com',
 
     'http://www.howdju.com.s3-website-us-east-1.amazonaws.com',
     'http://pre-prod.www.howdju.com.s3-website-us-east-1.amazonaws.com',
