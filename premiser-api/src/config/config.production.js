65560f17729ef33f7ff89df890874b68508ff409
Carl G <carl.gieringer@gmail.com>
2017-08-25 22:34:42 -0400

    Update pre-prod-www domain

--- premiser-api/src/config/config.production.js
+++ premiser-api/src/config/config.production.js
@@ -4,11 +4,11 @@ module.exports = {
     'http://www.howdju.com',
     'https://www.howdju.com',
 
-    'http://pre-prod.www.howdju.com',
-    'https://pre-prod.www.howdju.com',
+    'http://pre-prod-www.howdju.com',
+    'https://pre-prod-www.howdju.com',
 
     'http://www.howdju.com.s3-website-us-east-1.amazonaws.com',
-    'http://pre-prod.www.howdju.com.s3-website-us-east-1.amazonaws.com',
+    'http://pre-prod-www.howdju.com.s3-website-us-east-1.amazonaws.com',
 
     // www.howdju.com distribution
     'http://d1fcdpsoaaf9i6.cloudfront.net',
