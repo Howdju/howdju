37241285a8ae8372be4c23b34e573dbf18d2571e
Carl Gieringer <carl.gieringer@gmail.com>
2021-04-07 10:09:18 -0700

    Improve Sentry error handling to ignore expected errors

--- premiser-ui/src/sentryInit.js
+++ premiser-ui/src/sentryInit.js
@@ -4,21 +4,50 @@ import { Integrations } from "@sentry/tracing"
 import config from './config'
 import some from 'lodash/some'
 import {Severity} from '@sentry/types'
+import {uiErrorTypes} from './uiErrors'
+import {apiErrorCodes} from 'howdju-common'
+
 
 export default () => {
   Sentry.init(assign({
     integrations: [new Integrations.BrowserTracing()],
     beforeSend(event, hint) {
       if (event.exception) {
-        if (some(event.exception.values, ex => !ex.mechanism.handled)) {
-          // Ask the user what happened for unhandled exceptions
-          Sentry.showReportDialog({eventId: event.event_id})
-        } else {
-          // handled exceptions are not errors
-          event.level = Severity.Info
-        }
+        event = handleExceptionEvent(event, hint)
       }
       return event
     },
   }, config.sentry))
+}
+
+function handleExceptionEvent(event, hint) {
+  let isUnexpectedError = false
+
+  // We should always handle errors. So if the error was unhandled, that's unexpected
+  if (some(event.exception.values, ex => !ex.mechanism.handled)) {
+    isUnexpectedError = true
+  }
+
+  switch(hint.originalException.errorType) {
+    // UI error types that we don't even want to report
+    case uiErrorTypes.COMMIT_EDIT_RESULT_ERROR:
+      return null
+    case uiErrorTypes.API_RESPONSE_ERROR:
+      switch(hint.originalException.body.errorCode) {
+        // Most API errors are expected
+        case apiErrorCodes.UNEXPECTED_ERROR:
+          isUnexpectedError = true
+      }
+  }
+
+  if (isUnexpectedError) {
+    // Only prompt the user when we don't know what is going on
+    Sentry.showReportDialog({eventId: event.event_id})
+    event.level = Severity.Error
+  } else {
+    // Expected exceptions are just informational
+    event.level = Severity.Info
+  }
+
+  return event
 }
