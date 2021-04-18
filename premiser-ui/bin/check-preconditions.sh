bfa6a36420528584596caec9f71f6c38d85dc35f
Carl Gieringer <carl.gieringer@gmail.com>
2021-04-18 12:28:04 -0700

    Test before deploying

--- premiser-ui/bin/check-preconditions.sh
+++ premiser-ui/bin/check-preconditions.sh
@@ -1,7 +1,20 @@
+#!/bin/sh
+
+set -e
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+package_dir="$(dirname "$script_dir")"
+root_dir="$(dirname "$package_dir")"
+check_dirs=(
+"$package_dir"
+"$root_dir"/howdju-test-common
+"$root_dir"/howdju-common
+"$root_dir"/howdju-client-common
+)
 
-[[ -z $(git status -s "$script_dir"/..) ]] || {
-  echo "There are uncommitted changes."
-  exit 1
-}
+for check_dir in "${check_dirs[@]}"; do
+  [[ -z $(git status -s "$check_dir") ]] || {
+    echo "There are uncommitted changes in $check_dir"
+    exit 1
+  }
+done
