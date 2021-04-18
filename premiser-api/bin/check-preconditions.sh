3840d6e14738f5772b22f1d47af0557fae979e8a
Carl Gieringer <carl.gieringer@gmail.com>
2021-04-18 13:03:47 -0700

    Check for uncommitted changes and run tests before deploying API

---
+++ premiser-api/bin/check-preconditions.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+package_dir="$(dirname "$script_dir")"
+root_dir="$(dirname "$package_dir")"
+check_dirs=(
+"$package_dir"
+"$root_dir"/howdju-test-common
+"$root_dir"/howdju-common
+"$root_dir"/howdju-service-common
+"$root_dir"/howdju-ops
+)
+
+for check_dir in "${check_dirs[@]}"; do
+  [[ -z $(git status -s "$check_dir") ]] || {
+    echo "There are uncommitted changes in $check_dir"
+    exit 1
+  }
+done
