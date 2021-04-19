708b26f3c8382780dd139fa6ad91197c6137e422
Carl Gieringer <carl.gieringer@gmail.com>
2021-04-18 20:38:07 -0700

    Also install the root project when deploying

--- bin/install-all.sh
+++ bin/install-all.sh
@@ -2,3 +2,6 @@
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 "$script_dir/do-all.sh" yarn install
+
+cd "$script_dir/.."
+yarn install
--- bin/reinstall-all.sh
+++ bin/reinstall-all.sh
@@ -3,3 +3,7 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 "$script_dir/do-all.sh" rm -rf node_modules
 "$script_dir/do-all.sh" yarn install
+
+cd "$script_dir/.."
+rm -rf node_modules
+yarn install
