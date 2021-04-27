64178c348e94a596c7316419a6be3df50e87d7e6
Carl Gieringer <carl.gieringer@gmail.com>
2021-04-26 21:23:29 -0700

    Remove heap analytics
    I wasn't using it, and it caused unhandled errors that stopped the JS
    debugger when I clicked on buttons.

--- premiser-ui/config/webpack.production.config.js
+++ premiser-ui/config/webpack.production.config.js
@@ -20,9 +20,6 @@ module.exports.htmlWebpackPluginConfig = {
   googleAnalytics: {
     trackingId: 'UA-104314283-1',
   },
-  heapAnalytics: {
-    trackingId: '2535348131',
-  },
   mixpanel: {
     trackingId: 'cfedfc23579bf718b9e5704f6f6d85bd',
   },
