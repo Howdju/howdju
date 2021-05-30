156676a32818dd83de669b432a4ecbd5faed02da
Carl Gieringer <carl.gieringer@gmail.com>
2021-05-29 20:52:11 -0700

    Fix lint

--- premiser-api/bin/esbuild-local-server.js
+++ premiser-api/bin/esbuild-local-server.js
@@ -46,7 +46,7 @@ esbuilder({
 function startServer() {
   // Require is relative to the current file
   const localServerDistPath = '../dist/local-server.js'
-  delete require.cache[require.resolve(localServerDistPath)];
+  delete require.cache[require.resolve(localServerDistPath)]
   const {app} = require(localServerDistPath)
   const port = devApiServerPort()
   return app.listen(port, () => {
