9a77a17c22d53282bd2f44172b92d9d9cda20244
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2022-07-22 23:36:41 -0700

    Follow initial instructions for configuring react-native-share-menu for Android

--- howdju-mobile-app/android/app/src/main/java/com/howdju/mobile/app/MainApplication.java
+++ howdju-mobile-app/android/app/src/main/java/com/howdju/mobile/app/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+// import com.meedan.ShareMenuPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -23,10 +24,11 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          // I think this is currently autolinked.
+          // packages.add(new ShareMenuPackage());
           return packages;
         }
 
