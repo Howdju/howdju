947c27fcc6d1b5258435d6c131d68c2c144cc2b7
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2022-09-11 11:32:29 -0700

    Handle current branch in check-pushed.sh

--- bin/check-pushed.sh
+++ bin/check-pushed.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+current_branch=$(git branch --show-current)
+
 # use `wc -m` (characters) instead of `wc -l` because when there's a single commit, there appears to be no new line,
 # and therefore `wc -l` is zero.
-if [[ $(git fetch origin && git log origin/master..master | wc -m) -ne 0 ]]; then
+if [[ $(git fetch origin && git log origin/${current_branch}..${current_branch} | wc -m) -ne 0 ]]; then
   echo "master has commits that are not pushed to origin."
   exit 1
 fi
