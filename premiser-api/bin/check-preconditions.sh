7f5ae47f21cfe913a2bc5a49f3613d5251634e63
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2022-09-11 12:32:55 -0700

    Fix API check-preconditions to fail for uncommitted files

--- premiser-api/bin/check-preconditions.sh
+++ premiser-api/bin/check-preconditions.sh
@@ -2,5 +2,11 @@
 
 set -e
 
-node ../bin/check-uncommitted.mjs
-yarn run check:pushed
+node ../bin/check-uncommitted.mjs || {
+  echo check-uncommitted failed.
+  exit 1
+}
+yarn run check:pushed || {
+  echo check:pushed failed.
+  exit 1
+}
