1d8401bf9ca2cacf7116f70ce11548484681bb8b
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2022-10-22 11:51:53 -0700

    Add script to remove fully-merged branches

---
+++ bin/delete-squash-merged-branches.sh
@@ -0,0 +1,8 @@
+# https://stackoverflow.com/a/56026209
+# https://github.com/not-an-aardvark/git-delete-squashed
+git checkout -q master &&\
+ git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do\
+   mergeBase=$(git merge-base master $branch) &&\
+   [[ $(git cherry master $(git commit-tree $(git rev-parse "$branch^{tree}") -p $mergeBase -m _)) == "-"* ]] &&\
+   git branch -D $branch;\
+  done
