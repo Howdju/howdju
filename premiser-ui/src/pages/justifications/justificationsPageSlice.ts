b78b062934b228b851bc3e946b2c94eb28d48cdc
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2022-10-25 22:45:34 -0700

    Remove saga-slice (#134)

---
+++ premiser-ui/src/pages/justifications/justificationsPageSlice.ts
@@ -0,0 +1,29 @@
+import { api } from "@/actions";
+import { createSlice } from "@reduxjs/toolkit";
+
+export const justificationsPageSlice = createSlice({
+  name: "justificationsPage",
+  initialState: {
+    isNewJustificationDialogVisible: false,
+    isFetching: false,
+  },
+  reducers: {
+    showNewJustificationDialog: (state) => {
+      state.isNewJustificationDialogVisible = true;
+    },
+    hideNewJustificationDialog: (state) => {
+      state.isNewJustificationDialogVisible = false;
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(api.fetchRootJustificationTarget, (state) => {
+      state.isFetching = true
+    })
+    builder.addCase(api.fetchRootJustificationTarget.response, (state) => {
+      state.isFetching = false
+    })
+  },
+});
+
+export default justificationsPageSlice.actions
+export const justificationsPage = justificationsPageSlice.reducer
