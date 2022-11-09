7c8c33812aa68ee02f640f4435f62934f677725a
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2022-11-09 08:39:37 -0800

    Add eslint-plugin-react-hooks

--- premiser-ui/.eslintrc.js
+++ premiser-ui/.eslintrc.js
@@ -3,4 +3,11 @@ module.exports = {
     "howdju",
     "howdju/react",
   ],
+  "plugins": [
+    "react-hooks"
+  ],
+  "rules": {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  }
 };
