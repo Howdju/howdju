10d8143114bfc00215a6fb801b2931893c052cf1
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2022-12-28 05:56:11 -0800

    Add text-summary and PR comment to coverage

--- bin/merge-coverage.sh
+++ bin/merge-coverage.sh
@@ -6,4 +6,4 @@ rm -rf coverage
 mkdir -p coverage/workspaces
 yarn workspaces foreach -Apv exec bash -c '[ ! -f coverage/coverage-final.json ] && exit 0 || cp coverage/coverage-final.json '$(pwd)'/coverage/workspaces/$(basename $(pwd))-coverage-final.json'
 yarn run nyc merge coverage/workspaces coverage/monorepo-coverage.json
-yarn run nyc report -t coverage --report-dir coverage/html --reporter=html-spa
+yarn run nyc report -t coverage --report-dir coverage/html --reporter=html-spa --reporter=text-summary
