562ec99e527cbd6674bd14d298633641c32b56bb
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2022-12-27 23:54:08 -0800

    Merge coverage

---
+++ bin/merge-coverage.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+set -e
+
+rm -rf coverage
+mkdir -p coverage/workspaces
+yarn workspaces foreach -Apv exec bash -c '[ ! -f coverage/coverage-final.json ] && exit 0 || cp coverage/coverage-final.json '$(pwd)'/coverage/workspaces/$(basename $(pwd))-coverage-final.json'
+yarn run nyc merge coverage/workspaces coverage/monorepo-coverage.json
+yarn run nyc report -t coverage --report-dir coverage/html --reporter=html-spa
