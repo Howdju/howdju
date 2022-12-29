c7465fce5765dd9d3e0fde837009a260fc830420
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2022-12-28 22:32:49 -0800

    Add CI coverage and artifact upload; all tests runInBand (#199)
    * Add CI coverage and artifact upload; all tests runInBand
    * Add relative coverage text-summary and PR comment to coverage

---
+++ bin/merge-coverage.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+set -e
+
+echo Deleting $(pwd)/coverage
+rm -rf $(pwd)/coverage
+echo Creating coverage/workspaces
+mkdir -p coverage/workspaces
+echo Copying coverage report from workspaces
+yarn workspaces foreach -Apv exec bash -c '[ ! -f coverage/coverage-final.json ] && exit 0 || cp coverage/coverage-final.json '$(pwd)'/coverage/workspaces/$(basename $(pwd))-coverage-final.json'
+echo Merging coverage reports
+yarn run nyc merge coverage/workspaces coverage/monorepo-coverage.json
