31356c264399fd43bc2ed82ea15a05526b1a6169
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2023-03-25 17:02:55 -0700

    PR edits
    Signed-off-by: Carl Gieringer <78054+carlgieringer@users.noreply.github.com>

--- howdju-common/lib/zodSchemaTypes.ts
+++ howdju-common/lib/zodSchemaTypes.ts
@@ -1,6 +1,6 @@
 import { Moment } from "moment";
 import { z } from "zod";
-import { JustificationOut, UserOut, WritOut, WritQuoteOut } from "./apiModels";
+import { UserOut, WritOut, WritQuoteOut } from "./apiModels";
 import { assert } from "./general";
 import { logger } from "./logger";
 
@@ -124,8 +124,6 @@ export type EntityName<T> = T extends Proposition
   : T extends CreatePropositionInput
   ? "Proposition"
   : T extends Justification
-  ? "Justification"
-  : T extends JustificationOut
   ? "Justification"
   : T extends CreateJustification
   ? "Justification"
