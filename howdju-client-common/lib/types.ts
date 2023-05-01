fb5e17ac440e2ec29eba37af16ba1ac76b318e78
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2023-05-01 08:16:42 -0700

    Fix WindowMessageHandler.handleEvent
    Signed-off-by: Carl Gieringer <78054+carlgieringer@users.noreply.github.com>

--- howdju-client-common/lib/types.ts
+++ howdju-client-common/lib/types.ts
@@ -11,3 +11,15 @@ export interface IframedAppMessage {
   source: WindowMessageSource;
   action: ExtensionFrameAction;
 }
+
+export function isIframedAppMessage(
+  event: MessageEvent<any>
+): event is MessageEvent<IframedAppMessage> {
+  if (!("data" in event)) {
+    return false;
+  }
+  if (!("source" in event.data) || typeof event.data !== "string") {
+    return false;
+  }
+  return true;
+}
