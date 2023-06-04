743d7c7108d5262af480d201434e884547f1d99a
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2023-06-04 08:25:32 -0700

    Catch more errors in check-pushed
    Signed-off-by: Carl Gieringer <78054+carlgieringer@users.noreply.github.com>

--- bin/check-pushed.sh
+++ bin/check-pushed.sh
@@ -3,10 +3,14 @@
 set -e
 
 current_branch=$(git branch --show-current)
+git fetch origin
+output=$(git log origin/${current_branch}..${current_branch})
 
 # use `wc -m` (characters) instead of `wc -l` because when there's a single commit, there appears to be no new line,
 # and therefore `wc -l` is zero.
-if [[ $(git fetch origin && git log origin/${current_branch}..${current_branch} | wc -m) -ne 0 ]]; then
-  echo "master has commits that are not pushed to origin."
+if [[ $(echo $output | wc -m) -ne 0 ]]; then
+  echo ${current_branch} has commits that are not pushed to origin.
   exit 1
 fi
+
+echo ${current_branch} is pushed to origin.
