a1c5c01819eaaf3f97db3ffb8b63c9298122f1f7
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2023-07-03 00:54:49 -0700

    Fix check-pushed script
    Signed-off-by: Carl Gieringer <78054+carlgieringer@users.noreply.github.com>

--- bin/check-pushed.sh
+++ bin/check-pushed.sh
@@ -5,13 +5,15 @@ set -e
 echo Checking if current branch is pushed to origin...
 
 current_branch=$(git branch --show-current)
+echo Current branch is ${current_branch}
 git fetch origin
 output=$(git log origin/${current_branch}..${current_branch})
 
 # use `wc -m` (characters) instead of `wc -l` because when there's a single commit, there appears to be no new line,
 # and therefore `wc -l` is zero.
-if [[ $(echo $output | wc -m) -ne 0 ]]; then
-  echo ${current_branch} has commits that are not pushed to origin.
+if [[ $(echo -n $output | wc -m) -ne 0 ]]; then
+  echo ${current_branch} has commits that are not pushed to origin:
+  echo "${output}"
   exit 1
 fi
 
