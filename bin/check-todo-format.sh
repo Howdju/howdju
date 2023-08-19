9ae6ad513be546d3f0e5fc105a4cbcbaca80c872
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2023-08-18 22:48:51 -0700

    Fix check TODO format script for GHA CI (#521)
    ---------
    
    Signed-off-by: Carl Gieringer <78054+carlgieringer@users.noreply.github.com>

--- bin/check-todo-format.sh
+++ bin/check-todo-format.sh
@@ -6,8 +6,14 @@ target=${2:-}
 
 # Equivalent for the whole repo:
 #  egrep --exclude=check-todo-format\.sh --exclude-dir=node_modules --exclude-dir=dist --exclude-dir=coverage --exclude-dir=Pods --exclude-dir=\.git -RI '\bTODO\b' . | egrep -v '\bTODO\((\d+,?)+\)'
-git diff $base $target ':(exclude)bin/check-todo-format.sh' ':(exclude).github/workflows/ci.yml'\
-  | grep '^+' | egrep '\bTODO\b' | egrep -v '\bTODO\((\d+,?)+\)'
+output=$(git diff $base $target ':(exclude)bin/check-todo-format.sh' ':(exclude).github/workflows/ci.yml')
+
+if [[ $? -ne 0 ]]; then
+ echo Error getting diff for TODO check
+ exit 1
+fi
+
+echo "${output}" | grep '^+' | egrep '\bTODO\b' | egrep -v '\bTODO\((\d+,?)+\)'
 
 # grep returns 0 if it found matches. It is an error if we found matches.
 if [ $? -eq 0 ] ; then
