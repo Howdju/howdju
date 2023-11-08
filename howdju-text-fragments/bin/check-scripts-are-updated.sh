c507c5a81570c8a677588d7a127511952e6cf586
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2023-11-07 20:49:41 -0800

    Fix checks
    Signed-off-by: Carl Gieringer <78054+carlgieringer@users.noreply.github.com>

--- howdju-text-fragments/bin/check-scripts-are-updated.sh
+++ howdju-text-fragments/bin/check-scripts-are-updated.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 esbuild src/rangeToFragment.ts --bundle --outfile=dist/tmp/rangeToFragment.js --platform=browser --target=es2016
 cmp --silent dist/rangeToFragment.js dist/tmp/rangeToFragment.js || {
   echo "rangeToFragment.js is not up to date. Please run \`yarn run build:range-to-fragment-script\` and commit the changes."
