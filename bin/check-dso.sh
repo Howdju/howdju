957fbed6c5926b261ca819136cffe79799bbbc44
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2024-12-21 16:02:39 -0800

    Replace git-hooks with husky
    Signed-off-by: Carl Gieringer <78054+carlgieringer@users.noreply.github.com>

--- bin/check-dso.sh
+++ bin/check-dso.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
-# The only parameter is a path to a file containing the message
-# https://git-scm.com/book/en/v2/Customizing-Git-Git-Hooks
 commit_msg_file=$1
+
+if [[ -z $commit_msg_file ]]; then
+  echo "Usage: $0 <commit_msg_file>"
+  exit 1
+fi
 
 egrep 'Signed-off-by:.+<.+@.+>' $commit_msg_file > /dev/null
 if [ $? -ne 0 ] ; then
