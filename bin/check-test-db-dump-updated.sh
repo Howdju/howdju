4344fc902dafd0492685aa3087a34841bd96664c
Carl Gieringer <78054+carlgieringer@users.noreply.github.com>
2024-12-21 16:13:02 -0800

    Update check-test-db-dump-updated.sh to work on linux
    Signed-off-by: Carl Gieringer <78054+carlgieringer@users.noreply.github.com>

--- bin/check-test-db-dump-updated.sh
+++ bin/check-test-db-dump-updated.sh
@@ -2,9 +2,15 @@
 
 set -eo pipefail
 
-# gfind on macOS: `brew install findutils`
-latest_migration_modification=$(gfind premiser-api/db/migrations -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f1 -d" ")
-db_dump_modification=$(gfind howdju-service-common/test-data/premiser_test_schema_dump.sql -printf '%T@ %p\n' | cut -f1 -d" ")
+# Use gfind on macOS (installed via `brew install findutils`) and find on Linux
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  FIND_CMD="gfind"
+else
+  FIND_CMD="find"
+fi
+
+latest_migration_modification=$($FIND_CMD premiser-api/db/migrations -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f1 -d" ")
+db_dump_modification=$($FIND_CMD howdju-service-common/test-data/premiser_test_schema_dump.sql -printf '%T@ %p\n' | cut -f1 -d" ")
 if [[ $latest_migration_modification > $db_dump_modification ]]; then
   echo "DB dump is out of date (latest migration: $latest_migration_modification > $db_dump_modification). Please apply all migrations to local DB, run 'yarn run create:test-db-dump', and commit the changes."
   exit 1
